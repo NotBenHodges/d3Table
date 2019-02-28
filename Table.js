cb46be4e2735c5fe64d0a2b5cc392119941c3675
Ben Hodges <ben.hodges@centre.edu>
2019-02-28 14:41:32 -0500

    Added javascript

--- Table.js
+++ Table.js
@@ -1,1 +1,20 @@
 
+var foodMaker = function(taste,cons,temp){
+  return{
+    taste:taste,
+    consistency:cons,
+    temperature:temp
+  }
+}
+
+var foodData = [
+  foodMaker("spicy","chewy","warm"),
+  foodMaker("bitter","soft","cold"),
+  foodMaker("sweet","crunchy","hot")
+]
+
+var foodTable = d3.select("body").append("table");
+var foodRows = foodTable.selectAll("tr")
+                        .data(foodData)
+                        .enter()
+                        .append("tr");
